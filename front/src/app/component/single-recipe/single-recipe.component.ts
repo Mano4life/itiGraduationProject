a1dcd03989ac1ad24e5ecb593fb323847693064a
Momen Mamdouh <momenhelmi8@gmail.com>
2024-09-17 12:18:48 +0300

    first commit

---
+++ front/src/app/component/single-recipe/single-recipe.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-single-recipe',
+  standalone: true,
+  imports: [],
+  templateUrl: './single-recipe.component.html',
+  styleUrl: './single-recipe.component.css'
+})
+export class SingleRecipeComponent {
+
+}
