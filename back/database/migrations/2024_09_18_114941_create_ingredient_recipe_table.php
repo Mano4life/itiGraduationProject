fa7ec4cf5372770c7a5ccf69174b0a5206fab9ba
Momen Mamdouh <momenhelmi8@gmail.com>
2024-09-18 14:58:37 +0300

    created pivot tables recipe_tag & ingredient_recipe

---
+++ back/database/migrations/2024_09_18_114941_create_ingredient_recipe_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Models\Ingredient;
+use App\Models\Recipe;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('ingredient_recipe', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Ingredient::class)->constrained()->cascadeOnDelete();
+            $table->foreignIdFor(Recipe::class)->constrained()->cascadeOnDelete();
+            // $table->decimal('quantity', 8, 2);
+            // $table->string('measurement_unit');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('recipe_ingredient');
+    }
+};
