1cbde8ead5ab7e218567979a28f2430073f54c1c
Momen Mamdouh <momenhelmi8@gmail.com>
2024-09-19 22:01:10 +0300

    finished tags base controller, finsihed all base controllers

--- back/app/Http/Controllers/TagController.php
+++ back/app/Http/Controllers/TagController.php
@@ -12,54 +12,57 @@ class TagController extends Controller
      */
     public function index()
     {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
+        $result = Tag::latest()->paginate(8);
+        return response()->json($result, 200);
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store()
     {
-        //
+        $data = request()->validate([
+            'name' => ['required', 'string', 'min:3']
+        ]);
+
+        $result = Tag::create($data);
+
+        return response()->json($result, 200);
+        //test redirection if needed.
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Tag $tag)
+    public function show(Tag $Tag)
     {
-        //
-    }
+        if(is_null($Tag)){
+            return response()->json(['message' => 'Tag not found'], 404);
+        }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Tag $tag)
-    {
-        //
+        return response()->json($Tag, 200);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Tag $tag)
+    public function update(Tag $Tag)
     {
-        //
+        $data = request()->validate([
+            'name' => ['required', 'min:3']
+        ]);
+
+        $result = $Tag->update($data);
+
+        return response()->json($result);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Tag $tag)
+    public function destroy(Tag $Tag)
     {
-        //
+        $Tag->delete();
+        return response()->json(['message'=>'deleted succesfully'], 200);
     }
 }
