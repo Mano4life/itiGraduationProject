d69be7fe28375da55889a8be3cad9ce7e7f4a5f1
Momen Mamdouh <momenhelmi8@gmail.com>
2024-09-19 21:49:35 +0300

    finished base controller of recipes, ingredients

--- back/app/Http/Controllers/IngredientController.php
+++ back/app/Http/Controllers/IngredientController.php
@@ -12,54 +12,57 @@ class IngredientController extends Controller
      */
     public function index()
     {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
+        $result = Ingredient::latest()->paginate(8);
+        return response()->json($result, 200);
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store()
     {
-        //
+        $data = request()->validate([
+            'name' => ['required', 'string', 'min:3']
+        ]);
+
+        $result = Ingredient::create($data);
+
+        return response()->json($result, 200);
+        //test redirection if needed.
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Ingredient $ingredient)
+    public function show(Ingredient $Ingredient)
     {
-        //
-    }
+        if(is_null($Ingredient)){
+            return response()->json(['message' => 'Ingredient not found'], 404);
+        }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Ingredient $ingredient)
-    {
-        //
+        return response()->json($Ingredient, 200);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Ingredient $ingredient)
+    public function update(Ingredient $Ingredient)
     {
-        //
+        $data = request()->validate([
+            'name' => ['required', 'min:3']
+        ]);
+
+        $result = $Ingredient->update($data);
+
+        return response()->json($result);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Ingredient $ingredient)
+    public function destroy(Ingredient $Ingredient)
     {
-        //
+        $Ingredient->delete();
+        return response()->json(['message'=>'deleted succesfully'], 200);
     }
 }
