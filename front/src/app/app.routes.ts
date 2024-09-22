f41a230a7b8c951b093e131ea6e2c6a604333734
Momen Mamdouh <momenhelmi8@gmail.com>
2024-09-23 02:38:00 +0300

    removed old routing and added router model for components

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -1,7 +1,15 @@
 import { Routes } from '@angular/router';
 import { SingleRecipeComponent } from './component/single-recipe/single-recipe.component';
+import { HomeComponent } from './component/home/home.component';
+import { RecipesComponent } from './component/recipes/recipes.component';
 
 export const routes: Routes = [
+  {
+    path: '', component: HomeComponent
+  },
+  {
+    path: 'recipes', component: RecipesComponent
+  },
   {
     path: 'recipes/recipe', component: SingleRecipeComponent
   }
