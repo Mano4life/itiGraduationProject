723eb4adefdc0d7bd1a17925195b904cc67baa6c
Mariam Safwat <100482871+mariemsafwat20@users.noreply.github.com>
2024-09-23 22:09:14 +0300

    Design the signIn page.

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -3,13 +3,14 @@ import { SingleRecipeComponent } from './component/single-recipe/single-recipe.c
 import { HomeComponent } from './component/home/home.component';
 import { RecipesComponent } from './component/recipes/recipes.component';
 import { RegisterComponent } from './component/register/register.component';
+import { LoginComponent } from './component/login/login.component';
 
 export const routes: Routes = [
   {
     path: '', component: HomeComponent
   },
   { path: 'signUp', component:RegisterComponent },
-  // { path: 'signIn', component:LoginComponent },
+  { path: 'signIn', component:LoginComponent },
   {
     path: 'recipes', component: RecipesComponent
   },
