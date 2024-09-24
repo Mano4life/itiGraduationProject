d465703571ed91555e9d8927824e34e35e1ce453
dannel13 <dannelmaged@gmail.com>
2024-09-25 00:24:13 +0300

    navbar&about

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AboutComponent } from './component/about/about.component';
+import { NavbarComponent } from './component/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,AboutComponent],
+  imports: [RouterOutlet,AboutComponent,NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
