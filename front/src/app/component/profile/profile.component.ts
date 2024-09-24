a562c84c7e58ab6edaa59c5ad3086e9d7cce39c1
Mano4life <momenhelmi8@gmail.com>
2024-09-24 23:32:16 +0300

    created profile component and finished its design, logic (WIP)

--- front/src/app/component/profile/profile.component.ts
+++ front/src/app/component/profile/profile.component.ts
@@ -1,20 +1,28 @@
 import { Component } from '@angular/core';
 import { RecipesService } from '../../core/services/recipes.service';
-import { Router, ActivatedRoute } from '@angular/router';
+import { Router, ActivatedRoute, RouterLink } from '@angular/router';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [],
+  imports: [NgFor, RouterLink],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css'
 })
 export class ProfileComponent {
+  recipes!: any;
   constructor(
     private recipesService: RecipesService,
     private router: Router,
     private activatedRoute: ActivatedRoute
   ) {}
+
+  ngOnInit(){
+    this.recipesService.getRecipes().subscribe((res)=>{
+      this.recipes = res;
+    });
+  }
 
 
   onRecipeClick(id: number) {
