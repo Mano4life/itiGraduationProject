312583bb3b8ec4f992a06f773d21320f3cb3edb1
Mano4life <momenhelmi8@gmail.com>
2024-09-24 23:31:41 +0300

    created add-recipe component and finished its design, added add-recipe and edit-profile to routes

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -1,11 +1,19 @@
 import { Routes } from '@angular/router';
 import { SingleRecipeComponent } from './component/single-recipe/single-recipe.component';
 import { RecipesComponent } from './component/recipes/recipes.component';
-import { RecipeFormComponent } from './recipe-form/recipe-form.component';
+import { ProfileComponent } from './component/profile/profile.component';
+import { EditProfileComponent } from './component/profile/edit-profile/edit-profile.component';
+import { AddRecipeComponent } from './component/add-recipe/add-recipe.component';
 
 export const routes: Routes = [
   {
-    path: 'recipes/form', component: RecipeFormComponent
+    path: 'profile', component: ProfileComponent
+  },
+  {
+    path: 'profile/edit-profile', component: EditProfileComponent
+  },
+  {
+    path: 'profile/add-recipe', component: AddRecipeComponent
   },
   {
     path: 'recipes', component: RecipesComponent
