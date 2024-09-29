28eb157c1f6e8402a91dad53f64ae7169f5adc1c
Momen Mamdouh <momenhelmi8@gmail.com>
2024-09-29 19:19:58 +0300

    changled front title to My Daily Recipies

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -11,6 +11,5 @@ import { FooterComponent } from "./component/footer/footer.component";
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'front';
-
+  title = 'My Daily Recipeies site';
 }
