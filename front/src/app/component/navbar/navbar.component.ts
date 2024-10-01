8b88096bbf42137b67f22e23c9508e61fd6333cb
dannel13 <dannelmaged@gmail.com>
2024-10-01 16:20:31 +0300

    resp

--- front/src/app/component/navbar/navbar.component.ts
+++ front/src/app/component/navbar/navbar.component.ts
@@ -14,6 +14,7 @@ import { SearchInputComponent } from "../search-input/search-input.component";
 })
 export class NavbarComponent {
   logo: string = 'assets/imgs/logo.png';
+  
 
   constructor() { }
 
