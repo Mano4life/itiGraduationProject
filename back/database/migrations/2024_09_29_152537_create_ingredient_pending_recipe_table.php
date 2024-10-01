674c8635898ef36cdd986c1d9f818f7f3746771b
Mano4life <momenhelmi8@gmail.com>
2024-10-01 21:37:46 +0300

    fixed migration bug calling pendingRecipe

--- back/database/migrations/2024_09_29_152537_create_ingredient_pending_recipe_table.php
+++ back/database/migrations/2024_09_29_152537_create_ingredient_pending_recipe_table.php
@@ -2,6 +2,7 @@
 
 use App\Models\Ingredient;
 use App\Models\pending_recipe;
+use App\Models\PendingRecipe;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -15,7 +16,7 @@ return new class extends Migration
     {
         Schema::create('ingredient_pending_recipe', function (Blueprint $table) {
             $table->foreignIdFor(Ingredient::class)->cascadeOnDelete();
-            $table->foreignIdFor(pending_recipe::class)->cascadeOnDelete();
+            $table->foreignIdFor(PendingRecipe::class)->cascadeOnDelete();
 
             $table->float('quantity');
             $table->string('measurement_unit');
