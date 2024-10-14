e9838caf9ef1fdf7aa2a2d622b93b0c51386c8f4
Mariam Safwat <100482871+mariemsafwat20@users.noreply.github.com>
2024-10-14 23:09:51 +0300

    edit style in public profile

--- front/src/app/component/profile/profile.component.ts
+++ front/src/app/component/profile/profile.component.ts
@@ -30,16 +30,13 @@ export class ProfileComponent {
       this.recipes = res;
     });
 
-    
       this.usersService.getUser().subscribe((res) => {
         this.user = res;
         if(this.user.role == 'premium' || this.user.role == 'admin'){
           this.isPremium = true;
-         
         }
         console.log("output",this.user)
       });
- 
   }
 
   onRecipeClick(id: number) {
