fb9ab3186f359602feaa381e1819cc20720130ed
Mano4life <momenhelmi8@gmail.com>
2024-10-15 17:26:17 +0300

    changed admin edit recipe to accept image file

--- front/src/app/component/admin/admin-edit-recipes/admin-edit-recipes.component.ts
+++ front/src/app/component/admin/admin-edit-recipes/admin-edit-recipes.component.ts
@@ -1,5 +1,12 @@
 import { Component } from '@angular/core';
-import { FormArray, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  FormArray,
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { RecipesService } from '../../../core/services/recipes/recipes.service';
 import { CommonModule } from '@angular/common';
@@ -7,38 +14,41 @@ import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-admin-edit-recipes',
   standalone: true,
-  imports: [FormsModule, ReactiveFormsModule,CommonModule,RouterLink],
+  imports: [FormsModule, ReactiveFormsModule, CommonModule, RouterLink],
   templateUrl: './admin-edit-recipes.component.html',
-  styleUrl: './admin-edit-recipes.component.css'
+  styleUrl: './admin-edit-recipes.component.css',
 })
 export class AdminEditRecipesComponent {
-  editForm!:FormGroup;
-  editRecipeId!:number;
+  editForm!: FormGroup;
+  editRecipeId!: number;
   editSingleRecipe!: any;
+  imageLink: any;
 
-  constructor(private routerActive:ActivatedRoute, private recipeService: RecipesService,private router:Router) {
-
-  }
+  constructor(
+    private routerActive: ActivatedRoute,
+    private recipeService: RecipesService,
+    private router: Router
+  ) {}
 
   ngOnInit() {
     this.editForm = new FormGroup({
-      id:new FormControl('',[Validators.required]),
+      id: new FormControl('', [Validators.required]),
       name: new FormControl('', [Validators.required, Validators.minLength(2)]),
-      Username:new FormControl('',[Validators.required]),
-      Servings: new FormControl('', [Validators.required, Validators.minLength(1)]),
+      // Username:new FormControl('',[Validators.required]),
+      servings: new FormControl('', [
+        Validators.required,
+        Validators.minLength(1),
+      ]),
       time: new FormControl('', [Validators.required, Validators.minLength(1)]),
-      Description: new FormControl('', [
+      description: new FormControl('', [
         Validators.required,
         Validators.minLength(2),
       ]),
-      Direction: new FormControl('', [
+      directions: new FormControl('', [
         Validators.required,
         Validators.minLength(2),
       ]),
-      image: new FormControl('', [
-        Validators.required,
-        Validators.minLength(2),
-      ]),
+      image: new FormControl(null),
       category: new FormControl('', [
         Validators.required,
         Validators.minLength(2),
@@ -47,81 +57,122 @@ export class AdminEditRecipesComponent {
         Validators.required,
         Validators.minLength(2),
       ]),
-      ingredients: new FormArray([])
+      ingredients: new FormArray([]),
     });
-    this.editRecipeId = this.routerActive.snapshot.params['id']; 
+    this.editRecipeId = this.routerActive.snapshot.params['id'];
 
     if (this.editRecipeId) {
-      this.recipeService.getSingleRecipe(this.editRecipeId).subscribe((data) => {
-        this.editSingleRecipe = data;
-        console.log("recipe output",this.editSingleRecipe)
-        // Patch the form with the basic recipe data
-        this.editForm.patchValue({
-          id: data.id,
-          name: data.name,
-          Servings: data.servings,
-          time: data.time,
-          Description: data.description,
-          Direction: data.directions,
-          Username: data.user.name,
-          image: data.image,
-          category:data.category.name,
-          subcategory:data.subcategory.name,
-        });
-        console.log(this.editForm)
-    
-        // Clear existing ingredients and add new ones
-        const ingredientsFormArray = this.editForm.get('ingredients') as FormArray;
-        ingredientsFormArray.clear(); // Clear the existing FormArray
-    
-        data.ingredients.forEach((ingredient: { name: string; quantity: string; measurement_unit: string }) => {
-          const ingredientGroup = new FormGroup({
-            name: new FormControl(ingredient.name, Validators.required),
-            quantity: new FormControl(ingredient.quantity, Validators.required),
-            measurement_unit: new FormControl(ingredient.measurement_unit, Validators.required),
+      this.recipeService
+        .getSingleRecipe(this.editRecipeId)
+        .subscribe((data) => {
+          this.editSingleRecipe = data;
+          this.imageLink = data.image;
+          console.log('recipe output', this.editSingleRecipe);
+          console.log('image link is: ', this.imageLink);
+          // Patch the form with the basic recipe data
+          this.editForm.patchValue({
+            id: data.id,
+            name: data.name,
+            servings: data.servings,
+            time: data.time,
+            description: data.description,
+            directions: data.directions,
+            // Username: data.user.name,
+            // image: this.imageLink,
+            category: data.category.name,
+            subcategory: data.subcategory.name,
           });
-          ingredientsFormArray.push(ingredientGroup); // Add to FormArray
+          console.log(this.editForm);
+
+          // Clear existing ingredients and add new ones
+          const ingredientsFormArray = this.editForm.get(
+            'ingredients'
+          ) as FormArray;
+          ingredientsFormArray.clear(); // Clear the existing FormArray
+
+          data.ingredients.forEach(
+            (ingredient: {
+              name: string;
+              quantity: string;
+              measurement_unit: string;
+            }) => {
+              const ingredientGroup = new FormGroup({
+                name: new FormControl(ingredient.name, Validators.required),
+                quantity: new FormControl(
+                  ingredient.quantity,
+                  Validators.required
+                ),
+                measurement_unit: new FormControl(
+                  ingredient.measurement_unit,
+                  Validators.required
+                ),
+              });
+              ingredientsFormArray.push(ingredientGroup); // Add to FormArray
+            }
+          );
         });
-      });
-    }}
+    }
+  }
 
+  selectedFile: File | null = null;
+  onFileSelected(event: any) {
+    this.selectedFile = event.target.files[0] as File;
+  }
 
-  editRecipe(){
+  editRecipe() {
     if (!this.editForm.valid) {
       console.log('Form is invalid');
       for (const control in this.editForm.controls) {
         if (this.editForm.controls[control].invalid) {
-          console.log(`${control} is invalid:`, this.editForm.controls[control].errors);
+          console.log(
+            `${control} is invalid:`,
+            this.editForm.controls[control].errors
+          );
         }
       }
     }
-    
-    if (this.editForm.valid) {
-      const recipeData = {
-        name: this.editForm.value.name,
-        time:this.editForm.value.time,
-        servings:this.editForm.value.Servings,
-        description: this.editForm.value.Description,
-        directions: this.editForm.value.Direction,
-        image: this.editForm.value.image,
-        category: this.editForm.value.category,
-        subcategory: this.editForm.value.subcategory,
-        user_id: this.editSingleRecipe.user.id,
-        ingredients: this.editForm.value.ingredients.map(
-          (ingredient: {
-            name: any;
-            quantity: any;
-            measurement_unit: any;
-          }) => ({
-            name: ingredient.name,
-            quantity: ingredient.quantity,
-            measurement_unit: ingredient.measurement_unit,
-          })
-        ),
-      };
-      console.log("data to be sent",recipeData);
 
-      this.recipeService.updateRecipe(this.editRecipeId,recipeData).subscribe({
+    if (this.editForm.valid) {
+      const formData = new FormData();
+
+      // Append each field from recipeData to FormData
+      formData.append('name', this.editForm.value.name);
+      formData.append('time', this.editForm.value.time);
+      formData.append('servings', this.editForm.value.servings);
+      formData.append('description', this.editForm.value.description);
+      formData.append('directions', this.editForm.value.directions);
+
+      // Check if selectedFile is not null before appending
+      if (this.selectedFile) {
+        formData.append('image', this.selectedFile); // Ensure selectedFile is the File object
+      } else {
+        formData.append('image', this.imageLink);
+      }
+
+      formData.append('category', this.editForm.value.category);
+      formData.append('subcategory', this.editForm.value.subcategory);
+      formData.append('user_id', this.editSingleRecipe.user.id);
+
+      // Append each ingredient to FormData
+      this.editForm.value.ingredients.forEach(
+        (
+          ingredient: { name: any; quantity: any; measurement_unit: any },
+          index: number
+        ) => {
+          formData.append(`ingredients[${index}][name]`, ingredient.name);
+          formData.append(
+            `ingredients[${index}][quantity]`,
+            ingredient.quantity
+          );
+          formData.append(
+            `ingredients[${index}][measurement_unit]`,
+            ingredient.measurement_unit
+          );
+        }
+      );
+      console.log('data to be sent', formData);
+
+      this.recipeService.updateRecipe(this.editRecipeId, formData).subscribe({
         next: (res) => {
           console.log('Recipe added successfully:', res);
           this.router.navigate(['/admin']);
@@ -133,7 +184,5 @@ export class AdminEditRecipesComponent {
     }
   }
 
-  onImageUpload(img:any){
-
-  }
+  onImageUpload(img: any) {}
 }
